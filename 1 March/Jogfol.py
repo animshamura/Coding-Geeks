acd2652fc7fb3d74eae35b9b58ff0ad0db6a63ae
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2024-03-01 22:04:09 +0600

    Add files via upload

---
+++ 1 March/Jogfol.py
@@ -0,0 +1,4 @@
+t = int(input())
+for i in range(t):
+    n = input()
+    print("Sum =", int(n[0])+int(n[4]))
---
+++ 1 March/Odhogami.py
@@ -0,0 +1,7 @@
+c = 0
+for i in range(1000,0,-1): 
+    c+=1
+    print(i,end='\t')
+    if(c%5==0): print()
+    
+  
